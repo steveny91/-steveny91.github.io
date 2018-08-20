639b08777e4afa083e96de22164524a4b758c760
steveny91 <steven_yuen05@hotmail.com>
2018-08-20 10:26:50 -0400

    updated port

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
   Barba.Pjax.start();
 
   initializeScripts();
+});
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+
+// move next carousel
+$('.moveNextCarousel').click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $('.carousel').carousel('next');
+});
+
+// move prev carousel
+$('.movePrevCarousel').click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $('.carousel').carousel('prev');
 });
